0d44ef62a8a37dcd195e4ea118d27dead5380c1e
kabaakk <fb1907rsl@hotmail.com>
2020-02-24 19:15:33 +0300

    Android commerce
    Commerce app with android

---
+++ stok/app/src/main/java/com/example/stok/AdminCategoryActivity.java
@@ -0,0 +1,145 @@
+package com.example.stok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private ImageView tShirts,sportTShirts,femaleDresses,sweathers;
+    private ImageView glasses,hatscaps,walletsBagsPurses,shoes;
+    private ImageView headPhonesHandFree,Laptops,watches,mobilePhones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        tShirts = (ImageView) findViewById(R.id.t_shirts);
+        sportTShirts = (ImageView) findViewById(R.id.sports_t_shirts);
+        femaleDresses = (ImageView) findViewById(R.id.female_dresses);
+        sweathers = (ImageView) findViewById(R.id.sweathers);
+
+        glasses = (ImageView) findViewById(R.id.glasses);
+        hatscaps = (ImageView) findViewById(R.id.hats_caps);
+        walletsBagsPurses = (ImageView) findViewById(R.id.purses_bags_wallets);
+        shoes = (ImageView) findViewById(R.id.shoes);
+
+        headPhonesHandFree = (ImageView) findViewById(R.id.headphones_handfree);
+        Laptops = (ImageView) findViewById(R.id.laptop_pc);
+        watches = (ImageView) findViewById(R.id.watches);
+        mobilePhones = (ImageView) findViewById(R.id.mobilephones);
+
+
+        tShirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","tShirts");
+                startActivity(intent);
+            }
+        });
+
+        sportTShirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Sports tShirts");
+                startActivity(intent);
+            }
+        });
+
+        femaleDresses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Female Dresses");
+                startActivity(intent);
+            }
+        });
+
+        sweathers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Sweathers");
+                startActivity(intent);
+            }
+        });
+
+        glasses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Glasses");
+                startActivity(intent);
+            }
+        });
+
+        hatscaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Hats Caps");
+                startActivity(intent);
+            }
+        });
+
+        walletsBagsPurses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Wallets Bags Purses");
+                startActivity(intent);
+            }
+        });
+
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Ayakkabılar");
+                startActivity(intent);
+            }
+        });
+
+        headPhonesHandFree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","HeadPhones HandFree");
+                startActivity(intent);
+            }
+        });
+
+        Laptops.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Laptop");
+                startActivity(intent);
+            }
+        });
+
+        watches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Watches");
+                startActivity(intent);
+            }
+        });
+
+        mobilePhones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","Mobile Phones");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ stok/app/src/main/java/com/example/stok/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.stok.Prevalent;
+
+import com.example.stok.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
